f71508bea7e0d450a6788595e6d9510ea0d3fbb4
DiegoRamires <goncalves.ramires@gmail.com>
2020-08-12 21:39:43 -0300

    feature: add toggle input visible function and button

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, ScrollView, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 
 import styles from "./styles";
-import { TextInput } from "react-native-gesture-handler";
+import { TextInput, BorderlessButton } from "react-native-gesture-handler";
 
 function TeacherList() {
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+  function handleToggleFiltersVisible() {
+    setIsFilterVisible(!isFilterVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title='Proffys disponíveis'>
-        <View style={styles.searchForm}>
-          <Text style={styles.label}>Matéria</Text>
-          <TextInput style={styles.input} placeholder='Qual a matéria?' />
+      <PageHeader
+        title='Proffys disponíveis'
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name='filter' size={30} color='#fff' />
+          </BorderlessButton>
+        }
+      >
+        {isFilterVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput style={styles.input} placeholder='Qual a matéria?' />
 
-          <View style={styles.inputGroup}>
-            <View style={styles.inputBlock}>
-              <Text style={styles.label}>Dia da Semana</Text>
-              <TextInput style={styles.input} placeholder='Qual o dia?' />
-            </View>
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da Semana</Text>
+                <TextInput style={styles.input} placeholder='Qual o dia?' />
+              </View>
 
-            <View style={styles.inputBlock}>
-              <Text style={styles.label}>Horário</Text>
-              <TextInput style={styles.input} placeholder='Qual o horário?' />
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput style={styles.input} placeholder='Qual o horário?' />
+              </View>
             </View>
           </View>
-        </View>
+        )}
       </PageHeader>
 
       <ScrollView
--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -7,7 +7,7 @@ const style = StyleSheet.create({
   },
 
   teacherList: {
-    marginTop: -60,
+    marginTop: -20,
   },
 
   searchForm: {
